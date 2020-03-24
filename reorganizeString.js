5b0aa2a599a641f8b8fc4c92e2032261d316ef79
Tom <tomhere@gmail.com>
2020-03-23 19:28:47 -0700

    save changes

--- reorganizeString.js
+++ reorganizeString.js
@@ -17,9 +17,7 @@ S will consist of lowercase letters and have length in range [1, 500].
 */
 
 var reorganizeString = function(S) {
-  console.log(S.length);
   var letters = {};
-  var current = '';
   var result = '';
   for (i of S) {
     if (letters[i]) {
@@ -30,52 +28,41 @@ var reorganizeString = function(S) {
   }
   letters = Object.entries(letters);
   letters.sort((a, b) => b[1] - a[1]);
-  console.log(JSON.stringify(letters));
-  while (letters.length) {
-    if (letters[0][0] !== current) {
-      result += letters[0][0];
-      current = letters[0][0];
-      if (!--letters[0][1]) letters.shift();
-    } else {
-      if (letters[1] !== undefined) {
-        result += letters[1][0];
-        current = letters[1][0];
-        if (!--letters[1][1]) letters.splice(1, 1);
+  // console.log(JSON.stringify(letters));
+  // console.log(letters.length);
+  var prev = '';
+  while (result.length < S.length) {
+    for (var i = 0; i < letters.length; i++) {
+      if (letters[i][0] === prev) return '';
+      prev = letters[i][0];
+      result += letters[i][0];
+      if (!--letters[i][1]) {
+        letters = letters.slice(0, i).concat(letters.slice(i + 1));
       } else {
-        // console.log(result.length);
-        // return result;
-        var prev = '';
-        var found = false;
-        for (var i = 0; i < result.length; i++) {
-          prev = result[i-1] || '';
-
-          if (letters[0][0] !== result[0] && letters[0][0] !== prev) {
-            
-            result = result.slice(i - 1, i) + letters[0][0] + result.slice(i);
-            if (!--letters[0][1]) letters.shift();
-            found = true;
+        if (letters[i + 1] !== undefined) {
+          if (letters[i + 1][1] === 1 && letters[i][1] > 1) {
+            i++;
+            if (letters[i][0] === prev) return '';
+            prev = letters[i][0];
+            result += letters[i][0];
+            if (!--letters[i][1]) {
+              letters = letters.slice(0, i).concat(letters.slice(i + 1));
+              break;
+            }
           }
-          
         }
-        if (!found) {
-          console.log(result, result.length);
-          console.log(JSON.stringify(letters));
-          return 'not found';
-        }
-
-
       }
     }
   }
-  // console.log(result.length);
   return result;
 }
 
 
-//  console.log(reorganizeString('aaab'));
+// console.log(reorganizeString('aaab'));
 // console.log(reorganizeString('aab'));
 // console.log(reorganizeString('vvvlo'));
 // console.log(reorganizeString('blflxll'));
 // console.log(reorganizeString('ogccckcwmbmxtsbmozli'));
 // console.log(reorganizeString('nlmxhnpifuaxinxpxlcttjnlggmkjioewbecnofqpvcikiazmn'));
-console.log(reorganizeString('tndsewnllhrtwsvxenkscbivijfqnysamckzoyfnapuotmdexzkkrpmppttficzerdndssuveompqkemtbwbodrhwsfpbmkafpwyedpcowruntvymxtyyejqtajkcjakghtdwmuygecjncxzcxezgecrxonnszmqmecgvqqkdagvaaucewelchsmebikscciegzoiamovdojrmmwgbxeygibxxltemfgpogjkhobmhwquizuwvhfaiavsxhiknysdghcawcrphaykyashchyomklvghkyabxatmrkmrfsppfhgrwywtlxebgzmevefcqquvhvgounldxkdzndwybxhtycmlybhaaqvodntsvfhwcuhvuccwcsxelafyzushjhfyklvghpfvknprfouevsxmcuhiiiewcluehpmzrjzffnrptwbuhnyahrbzqvirvmffbxvrmynfcnupnukayjghpusewdwrbkhvjnveuiionefmnfxao'));
+// console.log(reorganizeString('tndsewnllhrtwsvxenkscbivijfqnysamckzoyfnapuotmdexzkkrpmppttficzerdndssuveompqkemtbwbodrhwsfpbmkafpwyedpcowruntvymxtyyejqtajkcjakghtdwmuygecjncxzcxezgecrxonnszmqmecgvqqkdagvaaucewelchsmebikscciegzoiamovdojrmmwgbxeygibxxltemfgpogjkhobmhwquizuwvhfaiavsxhiknysdghcawcrphaykyashchyomklvghkyabxatmrkmrfsppfhgrwywtlxebgzmevefcqquvhvgounldxkdzndwybxhtycmlybhaaqvodntsvfhwcuhvuccwcsxelafyzushjhfyklvghpfvknprfouevsxmcuhiiiewcluehpmzrjzffnrptwbuhnyahrbzqvirvmffbxvrmynfcnupnukayjghpusewdwrbkhvjnveuiionefmnfxao'));
+console.log(reorganizeString('kkkkzrkatkwpkkkktrq'));
