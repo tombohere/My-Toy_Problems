4aa8ab7603c59a286607f7ac6d57bc482d19d0a9
Tom <tomhere@gmail.com>
2020-03-27 13:28:23 -0700

    add changes

---
+++ isValidSudoku.js
@@ -0,0 +1,106 @@
+/*
+Determine if a 9x9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the 9 3x3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+A partially filled sudoku which is valid.
+
+The Sudoku board could be partially filled, where empty cells are filled with the character '.'.
+
+Example 1:
+
+Input:
+[
+  ["5","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: true
+Example 2:
+
+Input:
+[
+  ["8","3",".",".","7",".",".",".","."],
+  ["6",".",".","1","9","5",".",".","."],
+  [".","9","8",".",".",".",".","6","."],
+  ["8",".",".",".","6",".",".",".","3"],
+  ["4",".",".","8",".","3",".",".","1"],
+  ["7",".",".",".","2",".",".",".","6"],
+  [".","6",".",".",".",".","2","8","."],
+  [".",".",".","4","1","9",".",".","5"],
+  [".",".",".",".","8",".",".","7","9"]
+]
+Output: false
+Explanation: Same as Example 1, except with the 5 in the top left corner being 
+    modified to 8. Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+Note:
+
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+The given board contain only digits 1-9 and the character '.'.
+The given board size is always 9x9.
+*/
+
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+  var temp;
+  for (var i = 0; i < 9; i++) {
+    temp = board[i].join('').split('.').join('');
+    if (temp.length !== (new Set(temp)).size) return false;
+    temp = board.map(s => s[i]).join('').split('.').join('');
+    if (temp.length !== (new Set(temp)).size) return false;
+    temp = board.slice(i - i % 3, i - i % 3 + 3).map(s => s.slice(i % 3 * 3, i % 3 * 3 + 3)).join('').split(',').join('').split('.').join('');
+    if (temp.length !== (new Set(temp)).size) return false;
+  }
+  return true;
+};
+
+var board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+];
+console.log(isValidSudoku(board));
+
+board = [
+  ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+];
+console.log(isValidSudoku(board));
+
+board = [
+  [".", ".", ".", ".", "5", ".", ".", "1", "."],
+  [".", "4", ".", "3", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", "3", ".", ".", "1"],
+  ["8", ".", ".", ".", ".", ".", ".", "2", "."],
+  [".", ".", "2", ".", "7", ".", ".", ".", "."],
+  [".", "1", "5", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", "2", ".", ".", "."],
+  [".", "2", ".", "9", ".", ".", ".", ".", "."],
+  [".", ".", "4", ".", ".", ".", ".", ".", "."]
+]
+console.log(isValidSudoku(board));
