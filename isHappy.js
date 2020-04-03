328fe452dbfe4c4862c429cf0b5a9dfe3e113db5
Tom <tomhere@gmail.com>
2020-04-03 10:23:27 -0700

    add changes

--- isHappy.js
+++ isHappy.js
@@ -20,19 +20,59 @@ Explanation:
  */
 var isHappy = function(n) {
   var seq = {};
-  var str = n + '';
+  var str = n;
   var num;
   while (true) {
     num = 0;
-    for (var i of str) {
+    for (var i of str.toString()) {
       num += i * i;
     }
     if (num === 1) return true;
     if (seq[num] !== undefined) return false;
     seq[num] = 1;
-    str = num + '';
+    str = num;
   }
 };
 
-console.log(isHappy(19));
-console.log(isHappy(18));
+var isHappy2 = function(n) {
+  var s = n;
+  var num, tot;
+  while (true) {
+    tot = 0;
+    do {
+      num = s % 10;
+      s = (s - num) / 10;
+      tot +=  num * num;
+    } while (s > 0);
+    switch (tot) {
+      case 1:
+        return true;
+      case 4:
+      case 16:
+      case 37:
+      case 58:
+      case 89:
+      case 145:
+      case 42:
+      case 20:
+        return false;
+      default:
+        s = tot;
+    }
+  }
+};
+
+
+var d =(new Date()).getTime();
+var count = 0;
+for (var i = 0; i < 10000000; i++) {
+  if(isHappy2(i+1)) {
+    count++;
+  }
+}
+console.log((new Date()).getTime() - d);
+
+for ( var i = 0; i < 10; i++) {
+  console.log(i + 1, isHappy2(i + 1));
+}
+
---
+++ maxSubArray.js
@@ -0,0 +1,32 @@
+/*
+  Maximum Subarray
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    var max = -Infinity;
+    var sum = 0;
+    for (var i of nums) {
+      sum = Math.max(i, sum + i);
+      max = Math.max(max, sum);
+    }
+    return max;
+};
+
+var a = [-2,1,-3,4,-1,2,1,-5,4];
+//var a = [1, 2];
+//var a = [-1];
+console.log(maxSubArray(a));   // [4, -1, 2, 1]
