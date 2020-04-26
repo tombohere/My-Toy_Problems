09331765de632e457d3cd2dfdedb52afb80c01f7
Tom <tom_here@yahoo.com>
2020-04-25 22:19:46 -0700

    add changes

--- canJump.js
+++ canJump.js
@@ -37,7 +37,15 @@ var canJump = function(nums) {
   return true;
 };
 
-
+var canJump = function(nums) {
+  let last = nums.length - 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+      if (i + nums[i] >= last) {
+          last = i;
+      }
+  }
+  return lastPos === 0;
+};
 
 console.log(canJump([2,3,1,1,4])); // true
 console.log(canJump([3,2,1,0,4])); // false
